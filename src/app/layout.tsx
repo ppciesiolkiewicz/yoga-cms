564d51a6271e831351b800dbbecc02475dc25292
Piotr Ciesiolkiewicz <ppciesiolkiewicz@gmail.com>
2026-04-12 02:01:39 +0530

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,15 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "YogaCMS",
-  description: "CMS for yoga studios",
+  title: "Scraper",
+  description: "Get information about the Internet",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className="bg-gray-50 text-gray-900 antialiased">{children}</body>
